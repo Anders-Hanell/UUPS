26073baafed0ed3aa23f216b520089f2ae0ebcdd
anders.hanell <anders.hanell@neuro.uu.se>
2022-10-31 09:05:00 +0100

    Added more descriptions of the models.

--- OralAdmin_Description.js
+++ OralAdmin_Description.js
@@ -4,7 +4,18 @@ class OralAdmin_Description extends HTMLElement {
   connectedCallback() {
     this.id = "DescriptionContainer";
     this.innerHTML = `
-    Oral administration.
+    This model simulates repeated oral administration.
+    <br><br>
+    The tablet is modeled as a sphere with even distribuiton of the drug.<br>
+    In the model the radius of the tablet decreases linearly with time as the tablet dissolves.<br>
+    Release will thus be higher at first, when the surface area is large, and slower towards the end.<br>
+    Note that the total release rate can be the combination of several tablets, depending on the settings of the model.
+    <br><br>
+    Drug released from the tablet is modelled to be instantaneously absorbed, but modified by the bioavailability.
+    <br><br>
+    Drug elimination is modelled to occur by first order kinetics.
+    <br><br>
+    The entire body except for the gastro-intestinal tract is treated as a single compartment.
     `;
 
     MathJax.typeset();
